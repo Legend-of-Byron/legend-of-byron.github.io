f9d9e48b704975b87eccfabb531023116f443c68
Velberhand <82670953+velberhand@users.noreply.github.com>
2022-05-30 23:32:42 +0200

    Update index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,5 @@
 export const title = "Legend of Byron";
 export const summary = `
-    is a story set in the future of a young man named 
-    Byron as he goes on a journey of self discovery.
+    is a story about a man named Byron as he goes on a journey of self discovery.
 `;
 export const version = '0.0.1';
